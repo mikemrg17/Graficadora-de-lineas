e0338396dd7c3169a315cf44b7576aaa2d374714
Luiss333 <luis.0125@hotmail.com>
2021-06-14 04:43:27 -0500

    edit usuario

--- cliente/src/components/editusuario.js
+++ cliente/src/components/editusuario.js
@@ -32,6 +32,30 @@ class Editusuario extends React.Component {
       console.log("No se recibió el parámetro de id");
     }
   }
+  onQuestionChange(value) {
+    value ? console.log("Aceptado") : console.log("No aceptado");
+    this.setState(
+      {
+        nombre: value,
+      },
+      console.log(this.state.nombre)
+    );
+  }
+  editQuestion = (e) => {
+    e.preventDefault();
+    alert("Se editarán los cambios");
+    console.log("Objeto a pasar");
+    console.log(this.state);
+    axios
+      .post("http://localhost:8080/Crud_React/EditarUsuario", this.state)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.info(error);
+        console.log("Ha ocurrido un error al mandar los datos");
+      });
+  };
   render() {
     const { idUsuario, email, nombre, apellido, password } = this.state;
     return (
@@ -39,7 +63,13 @@ class Editusuario extends React.Component {
         <h3>Informacion del usuario</h3>
 
         <form className="formEditar">
-          nombre: <input type="text" name="nombre" value={this.state.nombre} />
+          nombre:{" "}
+          <input
+            type="text"
+            name="nombre"
+            value={this.state.nombre}
+            onChange={(e) => this.onQuestionChange(e.target.value)}
+          />
           <input type="Submit" className="secondary" value="Editar" />
         </form>
       </Container>
---
+++ servidorGraficadora/src/java/Api/EditarUsuario.java
@@ -0,0 +1,80 @@
+package Api;
+
+import com.mysql.jdbc.StringUtils;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Luis
+ */
+public class EditarUsuario extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String payloadRequest = getBody(request);
+        System.out.println(payloadRequest);
+
+        int row;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection db = DriverManager.getConnection("jdbc:mysql://localhost/graficadoraDeLineas","root", "1234");
+
+            PreparedStatement statement = db.prepareStatement("UPDATE usuarios SET nombre=? WHERE id=?");
+            statement.setString(payloadRequest);
+
+            row = statement.executeUpdate();
+        }catch (Exception ex) {
+            System.out.println("No se pudo editar el registro");
+            ex.printStackTrace();
+        }
+
+    }
+public static String getBody(HttpServletRequest request) throws IOException {
+
+    String body = null;
+    StringBuilder stringBuilder = new StringBuilder();
+    BufferedReader bufferedReader = null;
+
+    try {
+        InputStream inputStream = request.getInputStream();
+        if (inputStream != null) {
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            char[] charBuffer = new char[128];
+            int bytesRead = -1;
+            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                stringBuilder.append(charBuffer, 0, bytesRead);
+            }
+        } else {
+            stringBuilder.append("");
+        }
+    } catch (IOException ex) {
+        throw ex;
+    } finally {
+        if (bufferedReader != null) {
+            try {
+                bufferedReader.close();
+            } catch (IOException ex) {
+                throw ex;
+            }
+        }
+    }
+
+    body = stringBuilder.toString();
+    return body;
+}
+}
