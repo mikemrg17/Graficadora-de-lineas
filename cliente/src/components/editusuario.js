e193b9d29ea75608318e1c63fe5803a4e336864e
Luiss333 <luis.0125@hotmail.com>
2021-06-15 03:44:19 -0500

    crud admin terminado

--- cliente/src/components/editusuario.js
+++ cliente/src/components/editusuario.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Container } from "react-bootstrap";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
+import history from "./history";
 
 class Editusuario extends React.Component {
   state = {
@@ -14,14 +15,15 @@ class Editusuario extends React.Component {
 
   componentDidMount() {
     const qId = new URLSearchParams(window.location.search).get("id");
-    this.setState({ id: qId });
+    this.setState({ idUsuario: qId });
+
     if (qId) {
       console.log("Se recibió el parámetro de id");
       axios
         .get(`http://localhost:8080/GraficadoraDeLineas/UsuarioSol?id=${qId}`)
         .then((response) => {
-          const question = response.data[0];
-          this.setState({ ...question });
+          const newusuario = response.data[0];
+          this.setState({ ...newusuario });
           console.log("datos obtenidos");
         })
         .catch((error) => {
@@ -32,7 +34,17 @@ class Editusuario extends React.Component {
       console.log("No se recibió el parámetro de id");
     }
   }
-  onQuestionChange(value) {
+  EmailChange = (value) => {
+    value ? console.log("Aceptado") : console.log("No aceptado");
+    this.setState(
+      {
+        email: value,
+      },
+      console.log(this.state.email)
+    );
+  };
+
+  NameChange = (value) => {
     value ? console.log("Aceptado") : console.log("No aceptado");
     this.setState(
       {
@@ -40,39 +52,83 @@ class Editusuario extends React.Component {
       },
       console.log(this.state.nombre)
     );
-  }
-  editQuestion = (e) => {
+  };
+  SurnameChange = (value) => {
+    value ? console.log("Aceptado") : console.log("No aceptado");
+    this.setState(
+      {
+        apellido: value,
+      },
+      console.log(this.state.apellido)
+    );
+  };
+  PasswordChange = (value) => {
+    value ? console.log("Aceptado") : console.log("No aceptado");
+    this.setState(
+      {
+        password: value,
+      },
+      console.log(this.state.password)
+    );
+  };
+
+  Editarusuario = (e) => {
     e.preventDefault();
     alert("Se editarán los cambios");
     console.log("Objeto a pasar");
     console.log(this.state);
     axios
-      .post("http://localhost:8080/Crud_React/EditarUsuario", this.state)
+      .post(
+        "http://localhost:8080/GraficadoraDeLineas/EditarUsuario",
+        this.state
+      )
       .then((response) => {
         console.log(response);
+        history.goBack();
       })
       .catch((error) => {
         console.info(error);
         console.log("Ha ocurrido un error al mandar los datos");
       });
   };
-  render() {
-    const { idUsuario, email, nombre, apellido, password } = this.state;
-    return (
-      <Container className="MarginContainer">
-        <h3>Informacion del usuario</h3>
 
-        <form className="formEditar">
+  render() {
+    return (
+      <div className="mainContainerR">
+        <h1 className="title">Informacion del usuario</h1>
+
+        <form className="formEditarUsario" onSubmit={this.Editarusuario}>
+          email:{" "}
+          <input
+            type="text"
+            name="nombre"
+            value={this.state.email}
+            onChange={(e) => this.EmailChange(e.target.value)}
+          />
           nombre:{" "}
           <input
             type="text"
             name="nombre"
             value={this.state.nombre}
-            onChange={(e) => this.onQuestionChange(e.target.value)}
+            onChange={(e) => this.NameChange(e.target.value)}
           />
-          <input type="Submit" className="secondary" value="Editar" />
+          apellido:{" "}
+          <input
+            type="text"
+            name="nombre"
+            value={this.state.apellido}
+            onChange={(e) => this.SurnameChange(e.target.value)}
+          />
+          password:{" "}
+          <input
+            type="text"
+            name="nombre"
+            value={this.state.password}
+            onChange={(e) => this.PasswordChange(e.target.value)}
+          />
+          <input type="Submit" className="secondary" value="Editar Usuario" />
         </form>
-      </Container>
+      </div>
     );
   }
 }
--- servidorGraficadora/src/java/Api/EditarUsuario.java
+++ servidorGraficadora/src/java/Api/EditarUsuario.java
@@ -1,6 +1,6 @@
+
 package Api;
 
-import com.mysql.jdbc.StringUtils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,40 +11,87 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
  * @author Luis
  */
-public class EditarUsuario extends HttpServlet {
 
+public class EditarUsuario extends HttpServlet {
+    
+    private PrintWriter out;
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        out = response.getWriter();
+        response.setContentType("application/json");
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        StringBuilder json = new StringBuilder();
+            
+        //Primero obtenemos el Objeto JSON que viene del cliente y lo transformamos a String
         String payloadRequest = getBody(request);
-        System.out.println(payloadRequest);
-
+        System.out.println("Objeto es: " + payloadRequest);
+        
+        //Inicializamos el parser para transformar el String a JSONObject
+        JSONParser parser = new JSONParser();
+        
+        //Inicializamos el nuevo objeto json
+        JSONObject jsonUsuario = new JSONObject(); 
+        
+        //Parseamos el String a JSONObject y lo asignamos al JSONObject que lo almacenará
+        try {
+            jsonUsuario = (JSONObject) parser.parse(payloadRequest);
+        } catch (ParseException ex) {
+            Logger.getLogger(EditarUsuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Objeto JSON es: " + jsonUsuario);
+        //idUsuario
+        long idUsuario = (long) jsonUsuario.get("idUsuario");
+        System.out.println("El idUsuario es: " + idUsuario);
+        //Email
+        String email = (String) jsonUsuario.get("email");
+        System.out.println("El correo es: " + email);
+        //Nombre
+        String nombre = (String) jsonUsuario.get("nombre");
+        System.out.println("El nombre es: " + nombre);
+        //Apellido
+        String apellido = (String) jsonUsuario.get("apellido");
+        System.out.println("El apellido es: " + apellido);
+        //Password
+        String password = (String) jsonUsuario.get("password");
+        System.out.println("La contraseña es: " + password);
+        
         int row;
-        try{
+        try {
+            int contador=0;
             Class.forName("com.mysql.jdbc.Driver");
             Connection db = DriverManager.getConnection("jdbc:mysql://localhost/graficadoraDeLineas","root", "1234");
-
-            PreparedStatement statement = db.prepareStatement("UPDATE usuarios SET nombre=? WHERE id=?");
-            statement.setString(payloadRequest);
-
+            PreparedStatement statement = db.prepareStatement("UPDATE usuarios SET email=?, nombre=?, apellido=?, password=? WHERE idUsuario='"+idUsuario+"'");
+            statement.setString(1, email);
+            statement.setString(2, nombre);
+            statement.setString(3, apellido);
+            statement.setString(4, password);
             row = statement.executeUpdate();
-        }catch (Exception ex) {
-            System.out.println("No se pudo editar el registro");
+            System.out.println("Se modifico en la base de datos");
+            
+        } catch (Exception ex) {
+            System.out.println("No se pudo insertar el registro");
             ex.printStackTrace();
         }
-
+        out.write(json.toString());
     }
-public static String getBody(HttpServletRequest request) throws IOException {
+    
+    public static String getBody(HttpServletRequest request) throws IOException {
 
     String body = null;
     StringBuilder stringBuilder = new StringBuilder();
@@ -77,4 +124,5 @@ public static String getBody(HttpServletRequest request) throws IOException {
     body = stringBuilder.toString();
     return body;
 }
+
 }
